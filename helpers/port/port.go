1768f54ff9b61e5aadbff9375460ba47c2a94b0b
Sam <yablokov-s-a@mail.ru>
2024-05-06 20:41:03 +0300

    рефакторинг пакета port

---
+++ helpers/port/port.go
@@ -0,0 +1,16 @@
+package port
+
+import (
+	"os"
+
+	env "github.com/joho/godotenv"
+)
+
+func GetPort() string {
+	err := env.Load(".ENV")
+	if err != nil {
+		panic("Невозможно загрузить .ENV")
+	}
+
+	return os.Getenv("TODO_PORT")
+}
