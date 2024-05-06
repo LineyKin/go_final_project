50bc505e95d539ea0f5701bf3b11bc9a23612a9e
Sam <yablokov-s-a@mail.ru>
2024-05-06 20:22:09 +0300

    лог загрузки .ENV (использование паники)

--- helpers/port.go
+++ helpers/port.go
@@ -1,7 +1,6 @@
 package helpers
 
 import (
-	"log"
 	"os"
 
 	env "github.com/joho/godotenv"
@@ -10,7 +9,8 @@ import (
 func GetPort() string {
 	err := env.Load(".ENV")
 	if err != nil {
-		log.Fatal("Невозможно загрузить .ENV")
+		panic("Невозможно загрузить .ENV")
 	}
+
 	return os.Getenv("TODO_PORT")
 }
